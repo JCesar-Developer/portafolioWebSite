d93d154902123712ca72f33f6af4c6c59bf58028
JulitoxD <91586835+JulitoxD@users.noreply.github.com>
2022-06-30 09:29:14 +0200

    Tag v1.0

--- js/index.js
+++ js/index.js
@@ -41,45 +41,12 @@ const openTag = (idTag) => {
         })
 }
 
-console.log(document.URL);
-// function onHandle(){
-//     const name = document.getElementById("name").value;
-//     const email = document.getElementById("email").value; 
-//     const agreement = document.getElementById("agreement").value;
-//     const role = document.getElementById("role").value;
-//     const details = document.getElementById("details").value;
+window.addEventListener('load', (event) =>{
 
-//     console.log(
-//         name, '\n',
-//         email, '\n',
-//         agreement, '\n',
-//         role, '\n',
-//         details, '\n',
-//     )
+    const isSuccessfull = document.URL.includes('msg200');
 
-// }
+    if ( isSuccessfull ){
+        alert("The email successfully sent");
+    }
 
-/** Send a email message with the next form:  
-    SUBJETC: acuerdo - tipo de contrato
-    FROM: nombre
-    EMAIL: email
-    ACUERDO: acuerdo
-    ROL: rol
-    DETAILS OF THE OFFER:
-*/
-// function sendEmail(){
-//     Email.send({
-//         SecureToken : "979cfd88-dd1f-4732-9873-55cb924dfe9f",
-//         // Host : "smtp.gmail.com",
-//         // Username : "julioasto.92@gmail.com",
-//         // Password : "JuLi0.CeSaR",
-//         To : "julioasto.92@gmail.com",
-//         From : "you@isp.com",
-//         Subject : "This is the subject",
-//         Body : "And this is the body"
-//     }).then((success) => {
-//         alert("The email successfully sent")
-//     }).catch((error) => {
-//         alert(error);
-//     });
-// }
+});
